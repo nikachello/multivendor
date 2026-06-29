960050e94992c7b44bb122debd79903e0212823a
nikachello <nikain35@gmail.com>
2026-06-30 02:03:02 +0400

    fixed default user creation

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -24,7 +24,7 @@ export const auth = betterAuth({
     additionalFields: {
       role: {
         type: "string",
-        defaultValue: "customer",
+        defaultValue: "seller",
         input: true,
       },
     },
