c72bf3cebce7b20610e8f25dd982a73180b90086
nikachello <nikain35@gmail.com>
2026-07-01 10:11:35 +0400

    redirect fix

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,3 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
-export const { signIn, signUp, signOut, useSession, requestPasswordReset, resetPassword } = createAuthClient();
+export const { signIn, signUp, signOut, useSession, requestPasswordReset, resetPassword } = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+});
